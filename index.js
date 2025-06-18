3249f5f426393923130eafd8fe9d2b1f71732e5c
waqas rafiq <wm66179@gmail.com>
2025-06-18 23:32:05 +0500

    mysql query

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-import express from "express"
-import mysql from "mysql2/promise";
+const mysql = require("mysql2");
+const { faker } = require('@faker-js/faker');
 
-const db = await mysql.createConnection({
+
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password : 'waqas@sql321',
   database: 'frist_crud',
 });
 
-console.log("succes");
+try {
+
+  connection.query ("SHOW TABLES", (err, result) => {
+  if(err) throw err; 
+  console.log(result);
+  
+}) 
+  
+} catch (err) {
+  console.log(err);
+  
+}
+
+let getRandomUser = () => {
+  return {
+    Id: faker.datatype.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+}
