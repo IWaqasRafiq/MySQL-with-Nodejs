e84578891f7af3ca7de74d1b3f9910770d18d0e1
waqas rafiq <wm66179@gmail.com>
2025-06-23 21:17:35 +0500

    create crud routes

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const express = require("express");
 const app = express();
 let ejs = require('ejs')
 const Path = require("path");
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set("view engine", "ejs");
 app.set("views", Path.join(__dirname, "/views"));
@@ -17,6 +21,7 @@ const connection = mysql.createConnection({
   database: process.env.DATABASE,
 });
 
+// home page
 app.get("/", (req , res)=>{
   let q = `SELECT * FROM user`;
   try {
@@ -30,14 +35,79 @@ app.get("/", (req , res)=>{
 }
 });
 
+// create user page
+app.post("/user", (req, res)=>{
+  let {name, email} = req.body;
+  let q = `INSERT INTO user (username, email) VALUES ('${name}', '${email}')`;
+  try {
+  connection.query(q, (err, result) => {
+    if (err) throw err;
+    res.redirect("/");
+    // res.send("User added successfully");
+  });
+} catch (err) {
+  console.log(err);
+}
+});
+
+// user show page
+app.get("/user/:id", (req, res)=>{
+  let {id} = req.params;
+  let q = `SELECT * FROM user WHERE id = '${id}'`;
+  console.log(id);
+  try {
+  connection.query(q, (err, result) => {
+    if (err) throw err;
+    let user = result[0];
+    res.render("show.ejs", {user});
+  });
+} catch (err) {
+  console.log(err);
+}
+});
+
+// user edit page
 app.get("/user/:id/edit", (req, res)=>{
   let {id} = req.params;
   let q = `SELECT * FROM user WHERE id = '${id}'`;
+  console.log(id);
   try {
   connection.query(q, (err, result) => {
     if (err) throw err;
     let users = result[0];
     res.render("edit.ejs", {users});
+  });
+} catch (err) {
+  console.log(err);
+}
+});
+
+// update user
+app.patch("/user/:id", (req, res)=>{
+  let {id} = req.params;
+  let {name: newname, email: newemail} = req.body;
+  console.log(req.body);
+  let q = `UPDATE user SET username = '${newname}', email = '${newemail}' WHERE id = '${id}'`;
+  try {
+  connection.query(q, (err, result) => {
+    if (err) throw err;
+    res.redirect("/");
+    // res.send("User updated successfully");
+  });
+} catch (err) {
+  console.log(err);
+}
+});
+
+// delete user
+app.delete("/user/:id", (req, res)=>{
+  let {id} = req.params;
+  let q = `DELETE FROM user WHERE id = '${id}'`;
+  try {
+  connection.query(q, (err, result) => {
+    if (err) throw err;
+    res.redirect("/");
+    // res.send("User deleted successfully");
   });
 } catch (err) {
   console.log(err);
